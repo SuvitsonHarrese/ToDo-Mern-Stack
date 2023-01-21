ae26f6b3559ae8fd42859cb7cc6b8922aa5383a2
Suvitson <suvitson17@gmail.com>
2023-01-21 19:05:50 +0530

    mongodb connected

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
- const Connection = () => {
-    const MONGODB_URI = 'mongodb://harrese:<daniel@17>@ac-ulb13h8-shard-00-00.yt2jjtk.mongodb.net:27017,ac-ulb13h8-shard-00-01.yt2jjtk.mongodb.net:27017,ac-ulb13h8-shard-00-02.yt2jjtk.mongodb.net:27017/?ssl=true&replicaSet=atlas-13ks62-shard-0&authSource=admin&retryWrites=true&w=majority';
+const Connection = () => {
+    const MONGODB_URI = 'mongodb://harrese:daniel17@ac-ulb13h8-shard-00-00.yt2jjtk.mongodb.net:27017,ac-ulb13h8-shard-00-01.yt2jjtk.mongodb.net:27017,ac-ulb13h8-shard-00-02.yt2jjtk.mongodb.net:27017/?ssl=true&replicaSet=atlas-13ks62-shard-0&authSource=admin&retryWrites=true&w=majority';
     mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
     mongoose.connection.on('connected', () => {
         console.log('Database connected successfully');
@@ -9,7 +9,7 @@ import mongoose from "mongoose";
     mongoose.connection.on('disconnect', () => {
         console.log('Database Disconnected');
     })
-    mongoose.connection.on('error', () => {
+    mongoose.connection.on('error', (error) => {
         console.log('Error while connecting with the database ', error.message);
     })
 }
--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import  express  from "express";
 
+import Connection from "./database/db.js";
+
 
 const app=express();
 const PORT=8000;
 
-app.listen(PORT,()=>console.log('Your server is running success on Port ${PORT}'));
+Connection();
+
+app.listen(PORT,()=>console.log('Your server is running success on Port '+PORT));
