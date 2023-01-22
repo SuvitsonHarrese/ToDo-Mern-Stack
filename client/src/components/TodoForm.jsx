51e01ece24a45a6114e0a7cedd824c016edc16da
Suvitson <suvitson17@gmail.com>
2023-01-22 21:19:19 +0530

    Data successfully stored in mongodb

--- client/src/components/TodoForm.jsx
+++ client/src/components/TodoForm.jsx
@@ -1,8 +1,17 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { adNewTodo } from "../redux/actions";
+
 const TodoForm = () => {
   const [text, setText] = useState("");
+  const dispatch=useDispatch();
 
-  const onFormSubmit = () => {};
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    dispatch(adNewTodo(text));
+    setText('');
+  };
+
   const onInputChange = (e) => {
     console.log(e.target.value);
     setText(e.target.value);
@@ -14,6 +23,7 @@ const TodoForm = () => {
         className="input"
         placeholder="Enter new todo"
         onChange={onInputChange}
+        value={text}
       />
     </form>
   );
--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
-import  express  from "express";
-
+import express from "express";
+import cors from 'cors';
+import bodyParser from 'body-parser';
 import Connection from "./database/db.js";
+import route from "./routers/route.js";
 
+const app = express();
+app.use(cors());
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
+app.use('/', route);
 
-const app=express();
-const PORT=8000;
+const PORT = 8000;
 
 Connection();
 
-app.listen(PORT,()=>console.log('Your server is running success on Port '+PORT));
+app.listen(PORT, () => console.log(`Your server is running success on Port ${PORT}`));
